70484fdde0daac965d25462a1bddeb532ed88798
Андрей Киреев <a-kireev1989@mail.ru>
2020-02-24 21:52:40 +0300

    completion

---
+++ First_laba/First_laba/First_laba.cpp
@@ -0,0 +1,243 @@
+﻿#include "framework.h"
+#include "First_laba.h"
+
+#define MAX_LOADSTRING 100
+
+// Глобальные переменные:
+HINSTANCE hInst;                                // текущий экземпляр
+WCHAR szTitle[MAX_LOADSTRING];                  // Текст строки заголовка
+WCHAR szWindowClass[MAX_LOADSTRING];            // имя класса главного окна
+
+// Отправить объявления функций, включенных в этот модуль кода:
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+BOOL                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
+
+	// TODO: Разместите код здесь.
+
+	// Инициализация глобальных строк
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_FIRSTLABA, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
+
+	// Выполнить инициализацию приложения:
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
+
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_FIRSTLABA));
+
+	MSG msg;
+
+	// Цикл основного сообщения:
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return (int)msg.wParam;
+}
+
+
+
+//
+//  ФУНКЦИЯ: MyRegisterClass()
+//
+//  ЦЕЛЬ: Регистрирует класс окна.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+	WNDCLASSEXW wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_FIRSTLABA));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = CreateSolidBrush(RGB(0, 160, 255));
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_FIRSTLABA);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+	return RegisterClassExW(&wcex);
+}
+
+//
+//   ФУНКЦИЯ: InitInstance(HINSTANCE, int)
+//
+//   ЦЕЛЬ: Сохраняет маркер экземпляра и создает главное окно
+//
+//   КОММЕНТАРИИ:
+//
+//        В этой функции маркер экземпляра сохраняется в глобальной переменной, а также
+//        создается и выводится главное окно программы.
+//
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+	hInst = hInstance; // Сохранить маркер экземпляра в глобальной переменной
+
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
+}
+
+//
+//  ФУНКЦИЯ: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  ЦЕЛЬ: Обрабатывает сообщения в главном окне.
+//
+//  WM_COMMAND  - обработать меню приложения
+//  WM_PAINT    - Отрисовка главного окна
+//  WM_DESTROY  - отправить сообщение о выходе и вернуться
+//
+//
+bool left_key = false, right_key = false;
+int times_cl_l = 0, times_cl_r = 0;
+int x_koord = 0, y_koord = 0;
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		// Разобрать выбор в меню:
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		// TODO: Добавьте сюда любой код прорисовки, использующий HDC...
+		if (left_key == false && right_key == false) {
+			SelectObject(hdc, GetStockObject(DC_PEN));
+			SetDCPenColor(hdc, RGB(0, 160, 255));
+			SelectObject(hdc, CreateSolidBrush(RGB(150, 150, 0)));
+			Ellipse(hdc, 300, 300, 500, 500);
+		}
+		
+
+		if (left_key) {
+			//	PAINTSTRUCT pss;
+				//HDC hdcc = BeginPaint(hWnd, &pss);
+			SelectObject(hdc, GetStockObject(DC_PEN));
+			SelectObject(hdc, CreateSolidBrush(RGB(0, 160, 255)));
+			if (times_cl_l * 10 + x_koord <= 420) {
+				SetDCPenColor(hdc, RGB(0, 160, 255));
+				if (times_cl_l * 10 <= 100) {
+					Ellipse(hdc, x_koord, y_koord, times_cl_l * 10 + x_koord, times_cl_l * 10 + y_koord);
+				}
+				else {
+					Ellipse(hdc, x_koord, y_koord, 100 + x_koord, 100 + y_koord);
+				}
+			}
+			left_key = false;
+		}
+
+		if (right_key) {
+			SelectObject(hdc, GetStockObject(DC_PEN));
+			SetDCPenColor(hdc, RGB(0, 160, 255));
+			if (times_cl_l * 10 + x_koord >= 420) {
+				SelectObject(hdc, CreateSolidBrush(RGB(0, 160, 255)));
+				if (times_cl_l * 10 <= 100) {
+					Ellipse(hdc, x_koord, y_koord, times_cl_r * 10 + x_koord, times_cl_r * 10 + y_koord);
+				}
+				else {
+					Ellipse(hdc, x_koord, y_koord, 100 + x_koord, 100 + y_koord);
+				}
+			}
+			right_key = false;
+		}
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_LBUTTONDOWN:
+	{	
+		left_key = true;
+		x_koord = LOWORD(lParam);
+		y_koord = HIWORD(lParam);
+		if(times_cl_l != 10) {
+			++times_cl_l;
+		}
+		InvalidateRect(hWnd, NULL, false);
+	}
+	break;
+	case WM_RBUTTONDOWN:
+	{
+
+		right_key = true;
+		x_koord = LOWORD(lParam);
+		y_koord = HIWORD(lParam);
+		if (times_cl_r != 10) {
+			++times_cl_r;
+		}
+		InvalidateRect(hWnd, NULL, false);
+	}
+	break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
+}
+
+// Обработчик сообщений для окна "О программе".
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
+
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
+}
